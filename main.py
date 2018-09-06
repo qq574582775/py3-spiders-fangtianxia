e6aabc1703ba02ebb2189e79e94b960509095f41
lch <574582775@qq.com>
2018-09-06 23:57:25 +0800

    增加注释
    增加注释，创建目录方面还需要进行优化

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 
-res=requests.get('http://cd.esf.fang.com/house-a016418/')
-#print(res.text)
-soup = BeautifulSoup(res.text,"html.parser")
-houses = soup.select(".shop_list.shop_list_4 dl")
-#print(houses)
-#域名
-domain = "http://cd.esf.fang.com"
-for house in houses:
-    try:
-        print(domain+house.select(".clearfix a")[0]['href'])
-        #getHouseInfo(domain+house.select(".clearfix a")[0]['href'])
-    except Exception as e:
-        print("----->",e)
+
+def getHouseInfo(url):
+    #print(url)
+    info = {}
+    houseText = requests.get(url)
+    soup = BeautifulSoup(houseText.text, "html.parser")
+    houses = soup.select(".tr-line clearfix trl-item1 w146")
+
+
+if __name__ == '__main__':
+    res=requests.get('http://cd.esf.fang.com/house-a016418/')
+    #print(res.text)
+    soup = BeautifulSoup(res.text,"html.parser")
+    houses = soup.select(".shop_list.shop_list_4 .trl-item price_esf")
+    #print(houses)
+    #域名
+    domain = "http://cd.esf.fang.com"
+    for house in houses:
+        try:
+            url = domain+house.select(".clearfix a")[0]['href']
+            getHouseInfo(domain+house.select(".clearfix a")[0]['href'])
+        except Exception as e:
+            print("----->",e)
+
+
 
 
 #https://ke.qq.com/webcourse/index.html#cid=303312&term_id=100359427&taid=2117521956446416&vid=k142628kka6
